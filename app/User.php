7c2a1bb4cf88c4d8a842b663d8d7ca881de0213c
YERSON YORDY CAMPOS DE LA CRUZ <ycamposde@gmail.com>
2020-05-12 08:34:06 -0500

    Auth user DB Mongo

--- app/User.php
+++ app/User.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use App\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
